6e2771f63e396ce1f342e4e5620d9a3b8240074e
conor williams <conorwilliams@outlook.com>
2024-12-01 14:14:05 +0000

    day 1

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # AoC_2024
+
+KISS principled C++23 (no modules), file `==` self-contained solution.
+
+```sh
+make -S . -B build && cmake --build build
+```
+
+Solutions are in `./build/day_*`
--- src/day_1.cpp
+++ src/day_1.cpp
@@ -1,10 +1,80 @@
+#include <algorithm>
+#include <fstream>
+#include <map>
+#include <print>
+#include <ranges>
+#include <stdexcept>
+#include <string>
+#include <vector>
 
-#include <format>
-#include <iostream>
+struct Parsed {
+  std::vector<int> lhs;
+  std::vector<int> rhs;
+};
+
+auto parse(std::string const &fname) -> Parsed {
+
+  // Open the file for reading
+  std::ifstream file{fname};
+
+  if (!file.is_open()) {
+    throw std::runtime_error("Could not open file");
+  }
+
+  Parsed parsed;
+
+  int a = 0;
+  int b = 0;
+
+  while (file >> a >> b) {
+    parsed.lhs.push_back(a);
+    parsed.rhs.push_back(b);
+  }
+
+  return parsed;
+}
+
+namespace views = std::ranges::views;
 
 int main() {
 
-  std::cout << std::format("Hello, World!\n");
+  // Part I
+
+  Parsed p = parse("./inputs/day_1.txt");
+
+  std::ranges::sort(p.lhs);
+  std::ranges::sort(p.rhs);
+
+  auto abs_diff = [](int a, int b) { return std::abs(a - b); };
+
+  auto all_diffs = views::zip_transform(abs_diff, p.lhs, p.rhs);
+
+  auto delta = std::ranges::fold_left(all_diffs, 0, std::plus{});
+
+  std::println("Part  I: {}", delta);
+
+  if (delta != 1879048) {
+    throw std::runtime_error("Day 1 regressed");
+  }
+
+  // Part II
+
+  std::map<int, int> counts{};
+
+  for (auto x : p.rhs) {
+    counts[x] += 1;
+  }
+
+  auto sims =
+      p.lhs | views::transform([&counts](int x) { return x * counts[x]; });
+
+  auto sim = std::ranges::fold_left(sims, 0, std::plus{});
+
+  std::println("Part II: {}", sim);
+
+  if (sim != 21024792) {
+    throw std::runtime_error("Day 1 regressed");
+  }
 
   return 0;
 }
